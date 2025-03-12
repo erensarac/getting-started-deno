35dd881d606fdf261c4bf8d3ca135ea5284a17de
erensarac <erenkansarac@hotmail.com>
2025-03-13 00:06:41 +0300

    response hello world message with JSON and 200 status code

--- hello-world-http-server/server.ts
+++ hello-world-http-server/server.ts
@@ -1,7 +1,7 @@
 Deno.serve((_req) => {
-  const body = JSON.stringify({ message: "NOT FOUND" });
+  const body = JSON.stringify({ message: "Hello World!" });
   return new Response(body, {
-    status: 404,
+    status: 200,
     headers: {
       "content-type": "application/json; charset=utf-8",
     },
