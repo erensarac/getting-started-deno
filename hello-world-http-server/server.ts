8ad1da6e7999c077a3b5a252be85a3afde6e876a
erensarac <erenkansarac@hotmail.com>
2025-03-13 00:01:49 +0300

    response 404 status code with JSON body and custom header using deno

--- hello-world-http-server/server.ts
+++ hello-world-http-server/server.ts
@@ -1,16 +1,9 @@
-Deno.serve(async (req) => {
-  console.log("Method:", req.method);
-
-  const url = new URL(req.url);
-  console.log("Path:", url.pathname);
-  console.log("Query parameters:", url.searchParams);
-
-  console.log("Headers:", req.headers);
-
-  if (req.body) {
-    const body = await req.text();
-    console.log("Body:", body);
-  }
-
-  return new Response("Hello, World!");
+Deno.serve((_req) => {
+  const body = JSON.stringify({ message: "NOT FOUND" });
+  return new Response(body, {
+    status: 404,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+    },
+  });
 });
